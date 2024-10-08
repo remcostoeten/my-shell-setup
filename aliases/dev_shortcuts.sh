fa1db6360bb2c7849cec61c8f9e5150e3b02b11f
Remco Stoeten <remcostoeten@hotmail.com>
2024-10-09 01:42:04 +0200

    feat: add voice to clipboard script

---
+++ aliases/dev_shortcuts.sh
@@ -0,0 +1,1 @@
+alias rmall='rm -rf node_modules .next'
--- injector.bash
+++ injector.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source './aliases/alias_injector.sh'
+
 base_dir="$(dirname "${BASH_SOURCE[0]}")"
 function_dir="$base_dir/functions"
 library_dir="$base_dir/libraries"
@@ -9,16 +11,16 @@ declare -A sourced_counts
 declare -A failed_counts
 
 source_files() {
-    local dir="$1"
-    local type="$2"
-    while IFS= read -r -d '' file; do
-        if source "$file" 2>/dev/null; then
-            ((sourced_counts[$type]++))
-        else
-            echo "Failed to source $type: $(basename "$file")"
-            ((failed_counts[$type]++))
-        fi
-    done < <(find "$dir" -type f -name "*.bash" -print0)
+  local dir="$1"
+  local type="$2"
+  while IFS= read -r -d '' file; do
+    if source "$file" 2>/dev/null; then
+      ((sourced_counts[$type]++))
+    else
+      echo "Failed to source $type: $(basename "$file")"
+      ((failed_counts[$type]++))
+    fi
+  done < <(find "$dir" -type f -name "*.bash" -print0)
 }
 
 source_files "$function_dir" "functions"
@@ -31,11 +33,11 @@ total_sourced=0
 total_failed=0
 
 for type in "${types[@]}"; do
-    sourced=${sourced_counts[$type]:-0}
-    failed=${failed_counts[$type]:-0}
-    echo "Sourced $sourced $type. Failed to source $failed $type."
-    ((total_sourced += sourced))
-    ((total_failed += failed))
+  sourced=${sourced_counts[$type]:-0}
+  failed=${failed_counts[$type]:-0}
+  echo "Sourced $sourced $type. Failed to source $failed $type."
+  ((total_sourced += sourced))
+  ((total_failed += failed))
 done
 
 echo "Total: Sourced $total_sourced items. Failed to source $total_failed items."
