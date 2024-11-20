e3016ed33ed638761c8b9fc5eb60680b779a12fa
Remco Stoeten <remcostoeten@hotmail.com>
2024-11-20 19:38:09 +0100

    allow navigation withhout cd

---
+++ core-settings/core_injector.sh
@@ -0,0 +1,5 @@
+## allow navigating without cd.
+setopt AUTO_CD
+
+## alow typing pro to jump to dir
+hash -d pro=~/code/rollyourownauthh/
--- main_injector.sh
+++ main_injector.sh
@@ -6,8 +6,10 @@ BASE_DIR="/home/remcostoeten/projects/zsh-setup"
 source "${BASE_DIR}/alias/alias_injector.sh"
 source "${BASE_DIR}/packages/package-injector.sh"
 source "${BASE_DIR}/utils/utils_injector.sh"
-source "${BASE_DIR}/scripts/script_injector.sh"
 
+source "${BASE_DIR}/scripts/script_injector.sh"
+source "${BASE_DIR}/core-settings/core_injector.sh"
 source "${BASE_DIR}/initialize/_zsh-help-menu.sh"
 
 source "${BASE_DIR}/_welcome-message.sh"
+
